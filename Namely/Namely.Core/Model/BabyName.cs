1d99bc32e67ba12f27c1b5ce16c22a49d22b0e0b
Jacob Barnes <jmbarnes1987@gmail.com>
2017-10-09 17:18:41 -0600

    Added BabyName model and sample data

---
+++ Namely/Namely.Core/Model/BabyName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namely.Core.Model
+{
+    public class BabyName
+    {
+        public string Name { get; set; }
+        public string History { get; set; }
+        public List<string> NickNames { get; set; }
+        //Continue adding additional name attributes
+    }
+}
---
+++ Namely/Namely.Core/Repository/NameRepository.cs
@@ -0,0 +1,34 @@
+﻿using Namely.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namely.Core.Repository
+{
+    //using hard coded data for the time being.
+    public class NameRepository
+    {
+        private static List<BabyName> sampleBabyNames = new List<BabyName>()
+        {
+            new BabyName()
+            {
+                Name="Jacob",
+                NickNames= new List<string>() {"Jake", "TestNickName" }
+            },
+            new BabyName()
+            {
+                Name="Christianne",
+                NickNames= new List<string>() {"Chris", "Christi" }
+            },
+            new BabyName()
+            {
+                Name="Ezekiel",
+                NickNames= new List<string>() {"Zeke"}
+            }
+        };
+
+
+    }
+}
