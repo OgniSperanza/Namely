20b66d71b447a686024ba0f02c2caba7097ebf8f
Jacob Barnes <jmbarnes1987@gmail.com>
2017-10-10 10:10:17 -0600

    Added Baby Name view and activity. Modified main.axml to test different layouts.

---
+++ Namely/BabyNameDetailActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Namely
+{
+    //[Activity(Label = "BabyNameDetailActivity")]
+    [Activity(Label = "Baby Name Detail", MainLauncher = true)]
+    public class BabyNameDetailActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
--- Namely/Resources/Resource.Designer.cs
+++ Namely/Resources/Resource.Designer.cs
@@ -101,7 +101,10 @@ namespace Namely
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int BabyNameDetailView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
