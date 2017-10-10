dbc8534bc1de0f002d71b762624597422cbae2a6
Jacob Barnes <jmbarnes1987@gmail.com>
2017-10-10 10:57:31 -0600

    Added name detail view and started adding controls.

--- Namely/Resources/Resource.Designer.cs
+++ Namely/Resources/Resource.Designer.cs
@@ -60,32 +60,41 @@ namespace Namely
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int AddFNameButton = 2131034114;
-			
 			// aapt resource value: 0x7f050005
-			public const int AddMNameButton = 2131034117;
-			
-			// aapt resource value: 0x7f050001
-			public const int FirstNameEditText = 2131034113;
-			
-			// aapt resource value: 0x7f050000
-			public const int FirstNameTextView = 2131034112;
-			
-			// aapt resource value: 0x7f050004
-			public const int MiddleNameEditText = 2131034116;
-			
-			// aapt resource value: 0x7f050003
-			public const int MiddleNameTextView = 2131034115;
+			public const int AddFNameButton = 2131034117;
 			
 			// aapt resource value: 0x7f050008
-			public const int TestAppButton = 2131034120;
+			public const int AddMNameButton = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int FirstNameEditText = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int FirstNameTextView = 2131034115;
 			
 			// aapt resource value: 0x7f050007
-			public const int TestAppEditText = 2131034119;
+			public const int MiddleNameEditText = 2131034119;
 			
 			// aapt resource value: 0x7f050006
-			public const int TestAppTextView = 2131034118;
+			public const int MiddleNameTextView = 2131034118;
+			
+			// aapt resource value: 0x7f05000b
+			public const int TestAppButton = 2131034123;
+			
+			// aapt resource value: 0x7f05000a
+			public const int TestAppEditText = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int TestAppTextView = 2131034121;
+			
+			// aapt resource value: 0x7f050000
+			public const int babyNameTextView = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int nickNamesTextView = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int pronunciationTextView = 2131034113;
 			
 			static Id()
 			{
